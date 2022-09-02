c0fa055948c2477d34331b876727054997f93c39
Trevor Makes <the.trevor.makes@gmail.com>
2022-09-02 02:46:22 -0700

    Fix formatting

--- src/util.cpp
+++ src/util.cpp
@@ -6,10 +6,10 @@ namespace util {
 
 uint8_t reverse_bits(uint8_t b) {
   // https://stackoverflow.com/a/2602885
-   b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
-   b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
-   b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
-   return b;
+  b = (b & 0xF0) >> 4 | (b & 0x0F) << 4;
+  b = (b & 0xCC) >> 2 | (b & 0x33) << 2;
+  b = (b & 0xAA) >> 1 | (b & 0x55) << 1;
+  return b;
 }
 
 } // namespace util
