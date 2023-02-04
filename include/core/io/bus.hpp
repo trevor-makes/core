4cfd4b6bac4f55f0a57c06f6a04a143604579ad6
Trevor Makes <the.trevor.makes@gmail.com>
2023-02-03 23:48:43 -0800

    Re-add basic PortBus type

--- include/core/io/bus.hpp
+++ include/core/io/bus.hpp
@@ -61,7 +61,7 @@ struct Latch {
   }
 };
 
-// Derived type should define the following:
+// Derived types should define the following:
 // typename DATA_TYPE
 // typename ADDRESS_TYPE
 // static void write_bus(ADDRESS_TYPE addr, DATA_TYPE data)
@@ -69,9 +69,61 @@ struct Latch {
 // static void config_write()
 // static void config_read()
 // static void config_float()
-// TODO maybe add CRTP to enforce this
 struct BaseBus {
+  // Derived types can override to flush delayed writes (e.g. EEPROM page mode)
   static void flush_write() {}
+};
+
+// Parallel computer bus for interfacing with external devices
+// See read_bus comments on overriding if necessary
+template <typename ADDRESS, typename DATA, typename RE, typename WE>
+struct PortBus : BaseBus {
+  using ADDRESS_TYPE = typename ADDRESS::TYPE;
+  using DATA_TYPE = typename DATA::TYPE;
+
+  static void config_write() {
+    ADDRESS::config_output();
+    DATA::config_output();
+    RE::config_output();
+    WE::config_output();
+  }
+
+  static void config_read() {
+    ADDRESS::config_output();
+    DATA::config_input();
+    RE::config_output();
+    WE::config_output();
+  }
+
+  static void config_float() {
+    ADDRESS::config_input();
+    DATA::config_input();
+    RE::config_input();
+    WE::config_input();
+  }
+
+  static void write_bus(ADDRESS_TYPE addr, DATA_TYPE data) {
+    ADDRESS::write(addr);
+    WE::enable();
+    DATA::write(data);
+    WE::disable();
+  }
+
+  // This function can be overridden if:
+  // - ADDRESS is latched from DATA; use config_output/input as commented
+  // - tOE longer than 70ns results in corrupted read value
+  static DATA_TYPE read_bus(ADDRESS_TYPE addr) {
+    //DATA::config_output();
+    ADDRESS::write(addr);
+    //DATA::config_input();
+    RE::enable();
+    // Need at least one cycle delay for AVR read latency
+    // Second delay cycle has been sufficient for SRAM/EEPROM with tOE up to 70ns
+    util::nop<2>(); // insert two NOP delay cycles
+    const DATA_TYPE data = DATA::read();
+    RE::disable();
+    return data;
+  }
 };
 
 // Use CORE_ARRAY_BUS(array, address_t) to generate template parameters
